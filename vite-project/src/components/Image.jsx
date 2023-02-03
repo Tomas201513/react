5e314358d578d8686354aa8099b4f932d587deff
Tomas Beyene <94804547+Tomas201513@users.noreply.github.com>
2023-02-03 15:37:44 +0300

    component

---
+++ vite-project/src/components/Image.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import reactLogo from "../assets/react.svg"
+
+const Image = () => {
+  return (
+      <>
+             <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      </>
+  )
+}
+
+export default Image
